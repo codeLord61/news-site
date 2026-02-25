f76bd184595a42e57b90e1659ed33b860a6b2512
Mahin Abdullah <mahinabdullah961@gmail.com>
2026-02-26 02:50:17 +0600

    Updated database file, added pivot tables and foreign key constraints

--- packly_news.sql
+++ packly_news.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 24, 2026 at 10:25 AM
--- Server version: 8.0.44
--- PHP Version: 8.3.24
+-- Generation Time: Feb 25, 2026 at 08:45 PM
+-- Server version: 8.4.3
+-- PHP Version: 8.5.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,12 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `articles` (
   `id` int NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `excerpt` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `slug` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
-  `content` text COLLATE utf8mb4_general_ci,
-  `comment_id` int DEFAULT NULL,
-  `status` enum('draft','submitted','pending','approved','rejected','published') COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `excerpt` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `slug` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `status` enum('draft','submitted','pending','approved','rejected','published') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `reporter_id` int DEFAULT NULL,
   `managed_by` int DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -48,14 +47,47 @@ CREATE TABLE `articles` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `articles_categories`
+--
+
+CREATE TABLE `articles_categories` (
+  `article_id` int NOT NULL,
+  `category_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles_medias`
+--
+
+CREATE TABLE `articles_medias` (
+  `article_id` int NOT NULL,
+  `media_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles_tags`
+--
+
+CREATE TABLE `articles_tags` (
+  `article_id` int NOT NULL,
+  `tag_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `categories`
 --
 
 CREATE TABLE `categories` (
   `id` int NOT NULL,
-  `name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `slug` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` text COLLATE utf8mb4_general_ci NOT NULL
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `slug` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -69,7 +101,7 @@ CREATE TABLE `comments` (
   `article_id` int NOT NULL,
   `user_id` int NOT NULL,
   `managed_by` int DEFAULT NULL,
-  `content` text COLLATE utf8mb4_general_ci,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `is_approved` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -82,10 +114,10 @@ CREATE TABLE `comments` (
 
 CREATE TABLE `medias` (
   `id` int NOT NULL,
-  `file_url` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
-  `media_type` enum('image') COLLATE utf8mb4_general_ci NOT NULL,
-  `alt_text` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `uploaded_by` int NOT NULL,
+  `file_url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `media_type` enum('image') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `alt_text` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `uploaded_by` int DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -97,8 +129,8 @@ CREATE TABLE `medias` (
 
 CREATE TABLE `roles` (
   `id` int NOT NULL,
-  `name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -110,8 +142,8 @@ CREATE TABLE `roles` (
 
 CREATE TABLE `tags` (
   `id` int NOT NULL,
-  `name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `slug` varchar(150) COLLATE utf8mb4_general_ci NOT NULL
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `slug` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -122,15 +154,39 @@ CREATE TABLE `tags` (
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `role_id` int NOT NULL,
-  `avatar_path` text COLLATE utf8mb4_general_ci,
+  `avatar_path` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_bookmark_articles`
+--
+
+CREATE TABLE `users_bookmark_articles` (
+  `user_id` int NOT NULL,
+  `article_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_follow_categories`
+--
+
+CREATE TABLE `users_follow_categories` (
+  `user_id` int NOT NULL,
+  `category_id` int NOT NULL,
+  `followed_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Indexes for dumped tables
@@ -141,7 +197,30 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `articles`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `slug` (`slug`);
+  ADD UNIQUE KEY `slug` (`slug`),
+  ADD KEY `fk_articles_reporter` (`reporter_id`),
+  ADD KEY `fk_articles_editor` (`managed_by`);
+
+--
+-- Indexes for table `articles_categories`
+--
+ALTER TABLE `articles_categories`
+  ADD PRIMARY KEY (`article_id`,`category_id`),
+  ADD KEY `fk_articles_categories_category` (`category_id`);
+
+--
+-- Indexes for table `articles_medias`
+--
+ALTER TABLE `articles_medias`
+  ADD PRIMARY KEY (`article_id`,`media_id`),
+  ADD KEY `fk_articles_medias_media` (`media_id`);
+
+--
+-- Indexes for table `articles_tags`
+--
+ALTER TABLE `articles_tags`
+  ADD PRIMARY KEY (`article_id`,`tag_id`),
+  ADD KEY `fk_articles_tags_tag` (`tag_id`);
 
 --
 -- Indexes for table `categories`
@@ -155,13 +234,17 @@ ALTER TABLE `categories`
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_comments_article` (`article_id`),
+  ADD KEY `fk_comments_user` (`user_id`),
+  ADD KEY `fk_comments_editor` (`managed_by`);
 
 --
 -- Indexes for table `medias`
 --
 ALTER TABLE `medias`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_media_user` (`uploaded_by`);
 
 --
 -- Indexes for table `roles`
@@ -186,6 +269,20 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`),
   ADD KEY `fk_users_role` (`role_id`);
+
+--
+-- Indexes for table `users_bookmark_articles`
+--
+ALTER TABLE `users_bookmark_articles`
+  ADD PRIMARY KEY (`user_id`,`article_id`),
+  ADD KEY `fk_bookmark_article` (`article_id`);
+
+--
+-- Indexes for table `users_follow_categories`
+--
+ALTER TABLE `users_follow_categories`
+  ADD PRIMARY KEY (`user_id`,`category_id`),
+  ADD KEY `fk_follow_category` (`category_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -238,10 +335,66 @@ ALTER TABLE `users`
 --
 
 --
+-- Constraints for table `articles`
+--
+ALTER TABLE `articles`
+  ADD CONSTRAINT `fk_articles_editor` FOREIGN KEY (`managed_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_articles_reporter` FOREIGN KEY (`reporter_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+--
+-- Constraints for table `articles_categories`
+--
+ALTER TABLE `articles_categories`
+  ADD CONSTRAINT `fk_articles_categories_article` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_articles_categories_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `articles_medias`
+--
+ALTER TABLE `articles_medias`
+  ADD CONSTRAINT `fk_articles_medias_article` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_articles_medias_media` FOREIGN KEY (`media_id`) REFERENCES `medias` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `articles_tags`
+--
+ALTER TABLE `articles_tags`
+  ADD CONSTRAINT `fk_articles_tags_article` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_articles_tags_tag` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `fk_comments_article` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_comments_editor` FOREIGN KEY (`managed_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_comments_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `medias`
+--
+ALTER TABLE `medias`
+  ADD CONSTRAINT `fk_media_user` FOREIGN KEY (`uploaded_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `fk_users_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users_bookmark_articles`
+--
+ALTER TABLE `users_bookmark_articles`
+  ADD CONSTRAINT `fk_bookmark_article` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_bookmark_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users_follow_categories`
+--
+ALTER TABLE `users_follow_categories`
+  ADD CONSTRAINT `fk_follow_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_follow_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
